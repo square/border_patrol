5424ca183f0fa83b5e807cdade5afc27b01b1b10
Rob Olson <rob@squareup.com>
2011-05-22 23:53:21 -0700

    Remove custom rspec formatter
    This library does not have a large enough test suite to make this useful

--- border_patrol.gemspec
+++ border_patrol.gemspec
@@ -26,5 +26,4 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency("rake")
   s.add_development_dependency("rspec", "~> 1.3.0")
-  s.add_development_dependency("progressbar")
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,6 @@ require 'nokogiri'
 base_dir = File.expand_path("#{File.dirname(__FILE__)}/..")
 app_dirs = ['lib', 'lib/kaml_pen']
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
-
 app_dirs.each do |app_dir|
   Dir["#{base_dir}/spec/#{app_dir}/*.rb"].each do |f|
     f.sub!('/spec','')
