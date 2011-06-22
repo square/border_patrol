d48f010ab9f9e0d54d3e90920fd1f96dbec3cd1c
Zach Brock <zach@squareup.com>
2011-06-22 11:24:53 -0700

    Explicitly test common usage

--- spec/lib/border_patrol/region_spec.rb
+++ spec/lib/border_patrol/region_spec.rb
@@ -5,21 +5,19 @@ describe BorderPatrol::Region do
     BorderPatrol::Region.new.should be_a Set
   end
 
+  it "stores the polygons provided at initialization" do
+    region = BorderPatrol::Region.new([create_polygon, create_polygon(1), create_polygon(2)])
+    region.length.should == 3
+  end
+
   describe "#contains_point?" do
-    def create_polygon(start=0)
-      BorderPatrol::Polygon.new(
-        BorderPatrol::Point.new(start,start), 
-        BorderPatrol::Point.new(start + 10, start), 
-        BorderPatrol::Point.new(start + 10, start + 10), 
-        BorderPatrol::Point.new(start, start + 10))
-    end
     subject { BorderPatrol::Region.new(@polygons) }
 
     it "raises an argument error if contains_point? takes more than 3 arguments" do
       expect { subject.contains_point? }.to raise_exception ArgumentError
       expect { subject.contains_point?(1,2,3) }.to raise_exception ArgumentError
     end
-    
+
     it "returns true if any polygon contains the point" do
       point = BorderPatrol::Point.new(1,2)
       @polygons = [create_polygon, create_polygon(30)]
@@ -40,4 +38,13 @@ describe BorderPatrol::Region do
       subject.contains_point?(1,2).should be_true
     end
   end
+
+  def create_polygon(start=0)
+    BorderPatrol::Polygon.new(
+      BorderPatrol::Point.new(start,start),
+      BorderPatrol::Point.new(start + 10, start),
+      BorderPatrol::Point.new(start + 10, start + 10),
+      BorderPatrol::Point.new(start, start + 10))
+  end
+
 end
