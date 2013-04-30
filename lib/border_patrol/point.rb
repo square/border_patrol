7dec9cfc75b3d95ed7bd3371fba531c74f6bd1f7
Scott Gonyea <me@sgonyea.com>
2013-04-30 14:00:14 -0700

    Add "lon" as a helpful alias as well

--- lib/border_patrol/point.rb
+++ lib/border_patrol/point.rb
@@ -1,13 +1,15 @@
 module BorderPatrol
   class Point < Struct.new(:x, :y)
-    alias :lat  :y
-    alias :lat= :y=
-    alias :lng  :x
-    alias :lng= :x=
-
     alias :latitude   :y
     alias :latitude=  :y=
+    alias :lat        :y
+    alias :lat=       :y=
+
     alias :longitude  :x
     alias :longitude= :x=
+    alias :lng        :x
+    alias :lng=       :x=
+    alias :lon        :x
+    alias :lon=       :x=
   end
 end
