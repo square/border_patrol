ebb49006baeeb0f6d2cef85724f2cc24fe1ee657
Scott Gonyea <me@sgonyea.com>
2013-04-30 13:04:51 -0700

    add aliases so it's easier to pick out the lat / lng values

--- lib/border_patrol/point.rb
+++ lib/border_patrol/point.rb
@@ -1,4 +1,13 @@
 module BorderPatrol
   class Point < Struct.new(:x, :y)
+    alias :lat  :y
+    alias :lat= :y=
+    alias :lng  :x
+    alias :lng= :x=
+
+    alias :latitude   :y
+    alias :latitude=  :y=
+    alias :longitude  :x
+    alias :longitude= :x=
   end
 end
